42c26655057e2c2f5a198a3e7121698b123255db
Nicholas Melhado <n.melhado@gmail.com>
2018-07-15 11:29:37 -0400

    building web app, need to add functionality to buttons

--- calculator.js
+++ calculator.js
@@ -1,40 +1,66 @@
-let cur = 0
+let cur = 0;
+let temp = [];
+let build = true;
 
 const add = (num1,num2) => {
   if(num2) {
-    return num1 + num2;
+    cur = num1 + num2;
+  } else {
+    cur += num1;
   }
-  cur += num1;
-  return cur; 
+  return cur;
 }
 
 const minus = (num1,num2) => {
   if(num2) {
-    return num1 - num2;
+    cur = num1 - num2;
+  } else {
+    cur -= num1;
   }
-  cur -= num1;
-  return cur; 
+  return cur;
 }
 
 const mult = (num1,num2) => {
   if(num2) {
-    return num1 * num2;
+    cur = num1 * num2;
+  } else {
+    cur *= num1;
   }
-  cur *= num1;
-  return cur; 
+  return cur;
 }
 
 const div = (num1,num2) => {
   if(num2) {
-    return num1 / num2;
+    cur = num1 / num2;
+  } else {
+    cur /= num1;
   }
-  cur /= num1;
-  return cur; 
+  return cur;
 }
 
-module.exports =  {
+const clear = (num1,num2) => {
+  temp = [];
+  cur = 0;
+  return cur;
+}
+const computations = {
   add,
   minus,
   mult,
   div
-};
+}
+
+const compute = (fn,num1,num2) => {
+  fn(num1,num2);
+}
+
+if(typeof module !== 'undefined') {
+  module.exports =  {
+    add,
+    minus,
+    mult,
+    div,
+    compute,
+    clear
+  };
+}
--- test/calculator.spec.js
+++ test/calculator.spec.js
@@ -6,11 +6,14 @@ describe('calculator', () => {
   it('adds numbers', () => {
     expect(calculator.add(5,6)).to.eql(11);
   });
+  it('adds one number to the previous', () => {
+    expect(calculator.add(6)).to.eql(17);
+  });
   it('subtracts numbers', () => {
     expect(calculator.minus(5,6)).to.eql(-1);
   });
   it('multiplies numbers', () => {
-    expect(calculator.mult(5,6)).to.eql(30);
+    expect(calculator.mult(6)).to.eql(-6);
   });
   it('divides numbers', () => {
     expect(calculator.div(5,6)).to.eql(5/6);
